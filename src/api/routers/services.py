13ebcf9eebb957cdc45e418a4f977438e2143eaf
Josh Nuti <joshua.nuti@icloud.com>
2022-11-29 18:06:21 -0600

    Created services router

--- src/api/routers/services.py
+++ src/api/routers/services.py
@@ -1,23 +1,19 @@
 from fastapi import APIRouter
-from ..helpers.models import Service, ServiceIn
-from ..helpers.config import read_config, write_config
+from ..models.services import Service, ServiceIn
+from ..helpers.file import read_config, write_config
 
 router = APIRouter(
-    prefix="/services",
+    prefix="/config/services",
     tags=['Services']
 )
 
-@router.get(path='/services', response_model=list[Service])
+@router.get(path='/', response_model=list[Service])
 async def get_categories():
     return read_config()['services']
 
 
-@router.post('/services/', response_model=list[Service])
+@router.post('/', response_model=list[Service])
 async def new_service(service: ServiceIn):
     config = read_config()
-
-    print(service.dict())
-    
-    config['services'].append(service.dict())
 
     return write_config(config)
