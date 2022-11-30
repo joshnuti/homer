86f9d4453875209886d1581b4d7468be73bf3173
Josh Nuti <joshua.nuti@icloud.com>
2022-11-30 17:46:05 -0600

    Completed link endpoints

--- src/api/routers/links.py
+++ src/api/routers/links.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
-from ..models.link import Link
+from fastapi import APIRouter, HTTPException
+from ..models.link import Link, LinkIn, LinkModify
 from ..helpers.file import read_config, write_config
+from ..helpers.id import get_max_id, get_max_order
 
 router = APIRouter(
     prefix="/config/link",
@@ -9,46 +10,68 @@ router = APIRouter(
 
 @router.get('s', response_model=list[Link])
 async def get_links():
-    return read_config()['links']
+    return read_config().links
 
 @router.post('', response_model=Link)
-async def new_link(link: Link):
+async def new_link(link: LinkIn):
     config = read_config()
-    config['links'].append(link.dict())
+
+    if link.name in [x.name for x in config.links]:
+        raise HTTPException(409, f'Link with name "{link.name}" already exists')
+
+    link = Link(**link.dict())
     
-    links = write_config(config)['links']
-    filtered_links = list(filter(lambda x: x['name'] == link.name, links))
+    new_id = get_max_id(config.links) + 1
+    link.id = new_id
+    if link.order == None: link.order = get_max_order(config.links) + 1
 
-    if len(filtered_links) == 1:
-        return filtered_links[0]
-    else:
-        return None
-
-@router.get('/{name}', response_model=Link)
-async def get_link(name: str):
-    links = read_config()['links']
-    filtered_links = list(filter(lambda x: x['name'] == name, links))
-
-    if len(filtered_links) == 1:
-        return filtered_links[0]
-    else:
-        return None
+    config.links.append(link)
     
-@router.patch('/{name}', response_model=Link)
-async def patch_link(name: str, link: Link):
-    pass
+    links = write_config(config).links
+    link = list(filter(lambda x: x.id == new_id, links))[0]
 
-@router.delete('/{name}', response_model=Link)
-async def delete_link(name: str):
+    return link 
+
+@router.get('/{id}', response_model=Link | None)
+async def get_link(id: int):
+    links = read_config().links
+    link = list(filter(lambda x: x.id == id, links))
+
+    if len(link) == 1: return link[0]
+    else: raise HTTPException(404, f'Link with id {id} not found')
+    
+@router.patch('/{id}', response_model=Link)
+async def patch_link(id: int, link_modify: LinkModify):
     config = read_config()
-    removed_links = list(filter(lambda x: x['name'] == name, config['links']))
-    filtered_links = list(filter(lambda x: x['name'] != name, config['links']))
 
-    config['links'] = filtered_links
+    original_link = list(filter(lambda x: x.id == id, config.links))
+    if len(original_link) == 0: raise HTTPException(404, f'Link with id {id} not found')
+    original_link = original_link[0]
+
+    remaining_links = list(filter(lambda x: x.id != id, config.links))
+
+    link_modify = { k:v for (k,v) in link_modify.dict().items() if v != None }
+    link_temp = original_link.dict()
+    link_temp.update(link_modify)
+    link_temp = Link(**link_temp)
+
+    remaining_links.append(link_temp)
+    config.links = remaining_links
+
+    write_config(config)
+    return link_temp
+
+@router.delete('/{id}', response_model=Link)
+async def delete_link(id: int):
+    config = read_config()
+
+    removed_link = list(filter(lambda x: x.id == id, config.links))
+
+    if len(removed_link) == 0: raise HTTPException(404, f'Link with id {id} not found')
+    remaining_links = list(filter(lambda x: x.id != id, config.links))
+
+    config.links = remaining_links
 
     write_config(config)
 
-    if len(removed_links) == 1:
-        return removed_links[0]
-    else:
-        return None
+    return removed_link[0]
