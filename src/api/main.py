07aa7773cca9877e6b6baf36142b77232c2ec113
Josh Nuti <joshua.nuti@icloud.com>
2023-01-02 23:41:50 -0600

    Refactored on startup event

--- src/api/main.py
+++ src/api/main.py
@@ -1,38 +1,35 @@
 from fastapi import FastAPI, Security
 from .helpers.logging import logger
 from .helpers.security import authorize
-from .helpers.file import verify_config_exists, copy_defaults, add_id_and_order
-from .routers import config, links, replace, services
+from .helpers.file import verify_config_path, copy_defaults, write_config, read_config
+from .routers import config, file, links, services
+from .helpers.exceptions import EmptyFileError
 
 app = FastAPI(
     title="Homer API",
     version="Dev",
     description="Icon prefix: 'fas fa-'",
+    # dependencies=[Security(authorize)]
 )
 
-app.include_router(config.router, dependencies=[Security(authorize)])
-app.include_router(replace.router, dependencies=[Security(authorize)])
-app.include_router(links.router, dependencies=[Security(authorize)])
-app.include_router(services.router, )#dependencies=[Security(authorize)])
+app.include_router(config.router)
+app.include_router(file.router)
+app.include_router(links.router)
+app.include_router(services.router)
 
 
 @app.on_event("startup")
 def startup_event():
     logger.info("==== Starting API ===========")
 
-    if not verify_config_exists():
-        logger.info("config.yml file not find. Copying defaults.yml")
-
-        try:
-            copy_defaults()
-        except FileNotFoundError:
-            logger.error(
-                'Unable to copy defaults.yml to config.yml. Please use /config/replace to upload a config file')
-        except:
-            logger.error(
-                'Error trying to copy defaults.yml to config.yml. Please use /config/replace to upload a config file')
-            
-    add_id_and_order()
+    try:
+        write_config(None, read_config(None))
+    except FileNotFoundError:
+        logger.info('No file found. Writing defaults')
+        copy_defaults(None)
+    except EmptyFileError:
+        logger.info('Empty file found. Writing defaults')
+        copy_defaults(None)
 
 
 @app.on_event("shutdown")
