b9a46e4e7ccea592a3b96684546e2768357ca35b
Josh Nuti <joshua.nuti@icloud.com>
2023-01-02 23:42:51 -0600

    Added ItemModify class

--- src/api/models/services.py
+++ src/api/models/services.py
@@ -40,3 +40,14 @@ class ServiceModify(BaseModel):
     order: int | None = None
     icon: str | None = None
     # items: list[ItemIn] | None = []
+
+class ItemModify(BaseModel):
+    order: int | None = None
+    name: str | None = None
+    logo: str | None = None
+    icon: str | None = None
+    subtitle: str | None = None
+    url: str | None = None
+    type: str | None = None
+    target: str | None = None
+    background: str | None = None
